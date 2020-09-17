076222e686904a6db86a56872af174368158f58e
ashishkumar-mishra <ashish.mishra5908@gmail.com>
2020-09-16 21:36:19 -0700

    commit for greedy algorithm problems

---
+++ problems/src/com/apple/greedy/EgyptianFraction.java
@@ -0,0 +1,41 @@
+package com.apple.greedy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EgyptianFraction {
+
+	public List<String> getEgyptianFractions(int numerator, int denominator, final List<String> result) {
+		if (numerator == 0 || denominator == 0) {
+			return result;
+		}
+
+		if (denominator % numerator == 0) {
+			result.add("1/" + denominator / numerator);
+			return result;
+		}
+
+		if (numerator % denominator == 0) {
+			result.add("" + numerator / denominator);
+			return result;
+		}
+
+		if (numerator > denominator) {
+			result.add("" + numerator / denominator);
+			getEgyptianFractions(numerator % denominator, denominator, result);
+			return result;
+		}
+		// we reached at position where denominator > numerator and denominator %
+		// numerator is non zero
+		int n = denominator / numerator + 1;
+		result.add("1/" + n);
+		getEgyptianFractions(numerator * n - denominator, denominator * n, result);
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		EgyptianFraction egyptianFraction = new EgyptianFraction();
+		List<String> result = egyptianFraction.getEgyptianFractions(2,3, new ArrayList<>());
+		System.out.println(result);
+	}
+}
---
+++ problems/src/com/apple/greedy/JobSequence.java
@@ -0,0 +1,77 @@
+package com.apple.greedy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given an array of jobs where every job has a deadline and associated profit
+ * if the job is finished before the deadline. It is also given that every job
+ * takes single unit of time, so the minimum possible deadline for any job is
+ * one. How to maximize total profit if only one job can be scheduled at a time.
+ *
+ */
+public class JobSequence {
+
+	public char[] getjobSequence(List<Job> jobs) {
+
+		// sorting the jobs in descending order of profit
+
+		Collections.sort(jobs, (j1, j2) -> j2.profit - j1.profit);
+		// finding max deadline
+		int maxDeadline = 0;
+		for (Job job : jobs) {
+			if (job.getDeadline() > maxDeadline) {
+				maxDeadline = job.getDeadline();
+			}
+		}
+		char[] list = new char[maxDeadline];
+		boolean[] slots = new boolean[maxDeadline];
+
+		for (Job job : jobs) {
+			for (int j = job.getDeadline() - 1; j >= 0; j--) {
+				if (slots[j] == false) {
+					list[j] = job.getJobId();
+					slots[j] = true;
+					break;
+				}
+			}
+		}
+		return list;
+	}
+
+	class Job {
+		private char jobId;
+		private int deadline;
+		private int profit;
+
+		Job(char jobId, int deadline, int profit) {
+			this.jobId = jobId;
+			this.deadline = deadline;
+			this.profit = profit;
+		}
+
+		public char getJobId() {
+			return jobId;
+		}
+
+		public int getDeadline() {
+			return deadline;
+		}
+
+		public int getProfit() {
+			return profit;
+		}
+	}
+
+	public static void main(String[] args) {
+		JobSequence jobSequence = new JobSequence();
+		List<JobSequence.Job> jobs = new ArrayList<>();
+		jobs.add(jobSequence.new Job('a', 2, 100));
+		jobs.add(jobSequence.new Job('b', 1, 19));
+		jobs.add(jobSequence.new Job('c', 2, 27));
+		jobs.add(jobSequence.new Job('d', 1, 25));
+		jobs.add(jobSequence.new Job('e', 3, 15));
+		System.out.println(jobSequence.getjobSequence(jobs));
+	}
+}
